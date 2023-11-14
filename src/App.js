0843b11877ddf3bf33e93712e857c62279bd5db4
JulietaNoguera <julieta.noguera@estudiantes.unahur.edu.ar>
2023-11-13 21:04:02 -0300

    creado el login

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navegador from'./Vista/Globales/Nav'
 import Footer from './Vista/Globales/Footer'
+import Login from './Vista/Globales/Login';
 
 function App() {
   return (
     <>
       <Navegador></Navegador>
+      <Login></Login>
       <Footer></Footer>
     </>
   );
