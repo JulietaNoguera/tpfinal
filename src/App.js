d85d8aa803e37a90235d9130476cf7bb7d38e0b5
JulietaNoguera <julieta.noguera@estudiantes.unahur.edu.ar>
2023-11-13 21:43:39 -0300

    archivo contacto y nosotros

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navegador from'./Vista/Globales/Nav'
 import Footer from './Vista/Globales/Footer'
 import Login from './Vista/Globales/Login';
+import Nosotros from './Vista/Globales/Nosotros';
+import Contacto from './Vista/Globales/Contacto';
+
 
 function App() {
   return (
     <>
       <Navegador></Navegador>
       <Login></Login>
+      <Nosotros></Nosotros>
+      <Contacto></Contacto> 
       <Footer></Footer>
     </>
   );
